53d0a5a2106bb5ab4b8bd4e8a0fcaed78be9c7d0
miyamae <yu1ro.mya@gmail.com>
2021-09-01 15:29:01 +0900

    update: hello lambda ts

---
+++ sample/lambda/index.ts
@@ -0,0 +1,12 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export async function handler(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Hello, CDK! You've hit ${event.path}\n`,
+  };
+}
--- sample/lib/sample-stack.ts
+++ sample/lib/sample-stack.ts
@@ -1,15 +1,15 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
 import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class SampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, "HelloHandler", {
+    const hello = new NodejsFunction(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset("lambda"),
-      handler: "index.handler",
+      entry: "lambda/index.ts",
     });
 
     new apigw.LambdaRestApi(this, "Endpoint", {
