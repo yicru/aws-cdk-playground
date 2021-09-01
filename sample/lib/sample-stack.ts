2b94dc33b13e630eedc4bf777d7ac5d6fa6217b8
miyamae <yu1ro.mya@gmail.com>
2021-09-01 14:37:35 +0900

    add: hello lambda

--- sample/lib/sample-stack.ts
+++ sample/lib/sample-stack.ts
@@ -1,9 +1,19 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class SampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "index.handler",
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
